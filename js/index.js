e7cf63434b8f82eb6ac4ff5cf420db8caca57cdd
polinkhan <polinsir@gmail.com>
2022-04-21 18:23:50 +0600

    redesign

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
 let barDiv = document.querySelector(".bar");
 let nav_items = document.querySelector(".nav-items");
-let bar = document.querySelector(".fa-bars");
-let cls = document.querySelector(".fa-xmark");
+let bar = document.querySelector(".barIcon");
 let item = document.querySelector(".nav-items");
+let dom = document.querySelectorAll(".container");
+console.log(dom);
 
-let func = ()=> {
+let func = () => {
   nav_items.classList.toggle("show");
   bar.classList.toggle("hide");
-  cls.classList.toggle("hide");
-}
+};
 
 barDiv.addEventListener("click", func);
 item.addEventListener("click", func);
